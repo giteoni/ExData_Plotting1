9e490a45f3a101b78fa94adcfec7d0579bfa7e99
giteoni <33196026+giteoni@users.noreply.github.com>
2018-03-07 15:24:55 +0800

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,42 @@
+#####################################################################################################
+#Exploratory Data Analysis (Coursera)
+#Course Project 1
+#Plot 1
+#Mykhal Gideoni Mangada
+#####################################################################################################
+
+#clear contents 
+rm(list = ls())
+
+#specify URL and download file
+file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+dl_file <- download.file(file_url,destfile = "tmp")
+
+unzip("tmp")    #extract files
+unlink("tmp")   #delete file
+
+#Specify file name to be accessed, 
+#then read and store in a variable
+file_name <- "household_power_consumption.txt"
+data <- read.table(file_name, header = TRUE, sep = ";", na.strings = "?")
+
+#concatenate date and time, 
+#then convert to representable date and time
+data$dt <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+#Conveert to class Date
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#Store subset of data meeting the specified condition
+sub <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# Plot 1
+attach(data)
+
+hist(sub$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", 
+     ylab = "Frequency", main = "Global Active Power", breaks = 13, ylim = c(0,1300), xlim = c(0, 8))
+
+# Save file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
+detach(data)
